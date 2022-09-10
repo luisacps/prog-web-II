76dc06e49d0c5a3ae6717e80e3240c4d7f1fbbe8
Luísa C. Pimentel dos Santos <101952537+luisacps@users.noreply.github.com>
2022-09-10 11:16:50 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -1,30 +1,38 @@
 const tamanho = 40;
 let pecaId = 0;
+let posicaoInicial = 80;
+let posicaoFinal = 81;
+let classe = '';
 document.body.append(criarTabuleiro());
 
 function criarTabuleiro() {
-    const tam = 8;
+    const tam = 8
     let tabela = document.createElement('table');
 
-    tabela.style.borderStyle = 'solid';
-    tabela.style.borderSpacing = 0;
-    tabela.style.margin = 'auto';
+    tabela.style.borderStyle = 'solid'
+    tabela.style.borderSpacing = 0
+    tabela.style.margin = 'auto'
 
-    for (let i = 0; i < tam; i++) {
+    for (let i=0; i < tam; i++) {
         let linha = document.createElement('tr');
         tabela.append(linha);
-        for (let j = 0; j < tam; j++) {
+        for (let j=0; j < tam; j++) {
             let celula = document.createElement('td');
+            celula.setAttribute('id',i + 99);
             linha.append(celula);
 
             celula.style.width = `${tamanho}px`;
             celula.style.height = `${tamanho}px`;
+
+            pecaId += 1
+
             if (i % 2 == j % 2) {
                 celula.style.backgroundColor = 'black';
+                celula.setAttribute("class","droptarget");
                 if (i * 8 + j <= 24) {
-                    celula.append(criarPeca('black'));
+                    celula.append(criarPeca('black', pecaId));
                 } else if (i * 8 + j >= 40) {
-                    celula.append(criarPeca('blue'));
+                    celula.append(criarPeca('blue', pecaId));
                 }
             } else {
                 celula.style.backgroundColor = 'white';
@@ -34,10 +42,56 @@ function criarTabuleiro() {
     return tabela;
 }
 
-function criarPeca(cor) {
-    let imagem = document.createElement('img');
-    imagem.setAttribute('src', `img/${cor}.png`);
-    imagem.setAttribute('width', `${tamanho-4}px`);
-    imagem.setAttribute('height', `${tamanho-4}px`);
+function criarPeca(cor, ide) {
+        let imagem = document.createElement('img');
+
+        imagem.setAttribute('src', `img/${cor}.png`);
+        imagem.setAttribute('width', `${tamanho-4}px`);
+        imagem.setAttribute('height', `${tamanho-4}px`);
+        imagem.setAttribute('draggable','true');
+	    imagem.setAttribute('id', ide);
+	    imagem.setAttribute('class', cor);
+
     return imagem;
 }
+
+function dragstart(){
+	document.addEventListener("dragstart", function(event) {
+	  event.dataTransfer.setData("Text", event.target.id);
+	  posicaoInicial = event.path[1].id;
+	  classe = (event.path[0].className);
+	});
+}
+
+function dragend() {
+	document.addEventListener("dragend", function(event) {
+	});
+}
+
+function dragover() {
+	document.addEventListener("dragover", function(event) {
+	  event.preventDefault();
+	});
+}
+
+function drop(){
+	document.addEventListener("drop", function(event) {
+	event.preventDefault();
+	if ( event.target.className == "droptarget") {
+		const data = event.dataTransfer.getData("Text");
+		let c = event.path[0];
+		let t = c.childElementCount;
+		posicaoFinal = event.target.id;
+		if(t == '0' && posicaoInicial != posicaoFinal){
+			if(classe == 'blue' && posicaoInicial > posicaoFinal && posicaoInicial - posicaoFinal == 1|| classe == 'black' && posicaoInicial < posicaoFinal && posicaoFinal - posicaoInicial == 1) {
+				event.target.appendChild(document.getElementById(data));
+			}
+		}
+	}
+	});
+}
+
+dragstart();
+dragend();
+dragover();
+drop();
