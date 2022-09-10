41259478f2cedbf2766ab18a33c88ef4cebacf8f
Luísa C. Pimentel dos Santos <101952537+luisacps@users.noreply.github.com>
2022-09-10 09:40:12 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -1,35 +1,35 @@
-const tamanhoCelula = 40;
-let pecaId = 0;
-let localAtual = 80;
-let localFuturo = 81;
+const tamanho = 40;
+let idPeca = 0;
+let posicaoInicial = 80;
+let posicaoFinal = 81;
 let classe = '';
-document.body.append(criaTabuleiro());
+document.body.append(criarTabuleiro());
 
-function criaTabuleiro() {
-    const tamanho = 8;
+function criarTabuleiro() {
+    const tam = 8;
     let tabela = document.createElement('table');
 
     tabela.style.borderStyle = 'solid';
     tabela.style.borderSpacing = 0;
     tabela.style.margin = 'auto';
 
-    for (let i = 0; i < tamanho; i++) {
+    for (let i = 0; i < tam; i++) {
         let linha = document.createElement('tr');
         tabela.append(linha);
-        for (let j = 0; j < tamanho; j++) {
+        for (let j = 0; j < tam; j++) {
             let celula = document.createElement('td');
 			celula.setAttribute('id',i + 99);
             linha.append(celula);
-            celula.style.width = `${tamanhoCelula}px`;
-            celula.style.height = `${tamanhoCelula}px`;
-			pecaId += 1;
+            celula.style.width = `${tam}px`;
+            celula.style.height = `${tam}px`;
+			idPeca += 1;
             if (i % 2 == j % 2) {
                 celula.style.backgroundColor = 'black';
 				celula.setAttribute("class","droptarget");
                 if (i * 8 + j <= 24) {
-                    celula.append(criaPeca('black',pecaId));
+                    celula.append(criarPeca('black',idPeca));
                 } else if (i * 8 + j >= 40) {
-                    celula.append(criaPeca('red',pecaId));
+                    celula.append(criarPeca('blue',idPeca));
                 }
             } else {
                 celula.style.backgroundColor = 'white';
@@ -40,11 +40,11 @@ function criaTabuleiro() {
     return tabela;	
 }
 
-function criaPeca(cor,ide) {
+function criarPeca(cor,ide) {
 		let imagem = document.createElement('img');
 		imagem.setAttribute('src', `img/${cor}.png`);
-		imagem.setAttribute('width', `${tamanhoCelula-4}px`);
-		imagem.setAttribute('height', `${tamanhoCelula-4}px`);
+		imagem.setAttribute('width', `${tamanho-4}px`);
+		imagem.setAttribute('height', `${tamanho-4}px`);
 		imagem.setAttribute('draggable','true');
 		imagem.setAttribute('id', ide);
 		imagem.setAttribute('class', cor);
@@ -55,13 +55,14 @@ function criaPeca(cor,ide) {
 function dragstart(){
 	document.addEventListener("dragstart", function(event) {
 	  event.dataTransfer.setData("Text", event.target.id);
-	  localAtual = event.path[1].id;
+	  posicaoInicial = event.path[1].id;
 	  classe = (event.path[0].className);
 	});
 }
 
 function dragend() {
 	document.addEventListener("dragend", function(event) {
+        event.preventDefault()
 	});
 }
 
@@ -78,9 +79,9 @@ function drop(){
 		const data = event.dataTransfer.getData("Text");
 		let c = event.path[0];
 		let t = c.childElementCount;
-		localFuturo = event.target.id;
-		if(t == '0' && localAtual != localFuturo){
-			if(classe == 'red' && localAtual > localFuturo && localAtual - localFuturo == 1|| classe == 'black' && localAtual < localFuturo && localFuturo - localAtual == 1) {
+		posicaoFinal = event.target.id;
+		if(t == '0' && posicaoInicial != posicaoFinal){
+			if(classe == 'blue' && posicaoInicial > posicaoFinal && posicaoInicial - posicaoFinal == 1|| classe == 'black' && posicaoInicial < posicaoFinal && posicaoFinal - posicaoInicial == 1) {
 				event.target.appendChild(document.getElementById(data));
 			}
 		}
